9774ee54e43047eb9ca336c8b89c28b3bd6f7297
margolek <margolek555@gmail.com>
2020-12-14 10:08:24 +0100

    Add rest of code

--- Files_and_Exceptions/55.py
+++ Files_and_Exceptions/55.py
@@ -12,6 +12,38 @@ username.
 
 import json
 
+
 def get_stored_username():
 	"""Get stored username if available"""
-	
+	filename = 'username.json'
+	try:
+		with open(filename) as f_obj:
+			username = json.load(f_obj)
+	except FileNotFoundError:
+		return None
+	else:
+		return username
+
+def get_new_username():
+	"""Prompt for a new username."""
+	username = input('What is your name?')
+	filename = 'username.json'
+	with open(filename,'w') as f_obj:
+		json.dump(username,f_obj)
+	return username
+
+def greet_user():
+	"""Greet user by name"""
+	username = get_stored_username()
+	if username:
+		a = input('{} is your ID? (Y/n): '.format(username))
+		if a == 'Y':
+			print('Welcome back {} !'.format(username))
+		elif  a == 'n':
+			username = get_new_username()
+		
+	else:
+		username = get_new_username()
+		print('We will remember when you come back, {}!'.format(username))
+
+greet_user()
