353b5b1d080aa54cf3a438972d51c7d66e93c3c0
margolek <margolek555@gmail.com>
2020-12-22 00:28:37 +0100

    Create phone_number

---
+++ Regex/phone_number.py
@@ -0,0 +1,13 @@
+import re
+
+def example1():
+	my_pattern = re.compile(r'\d{9}')
+	my_number = my_pattern.search('In this text should be number and value is 997998778')
+	print(my_number.group())
+
+def example2():
+	my_pattern = re.compile(r'(\d{2}) (\d{3})')
+	my_number = my_pattern.search('Search number 33 345 in text')
+	print(my_number.group(2))
+
+example2()
